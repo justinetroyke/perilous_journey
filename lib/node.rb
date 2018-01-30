5130d5cf1d5a170dec69251759a94edd41e16b32
justinetroyke <jjtroyke@gmail.com>
2018-01-30 16:14:05 -0700

    update to test and added append method

--- lib/node.rb
+++ lib/node.rb
@@ -1,12 +1,18 @@
 require "pry"
 
 class Node
-  attr_reader :surname,
-              :next_node
 
   def initialize(surname)
     @surname = surname
     @next_node = nil
+  end
+
+  def surname
+    @surname
+  end
+
+  def next_node
+    @next_node
   end
 
 end
