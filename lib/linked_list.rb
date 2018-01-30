e5b3a4e6046e8554123f11c1564c39dc9ada6c43
justinetroyke <jjtroyke@gmail.com>
2018-01-30 16:13:07 -0700

    added test node is nil to linked list

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,7 +1,17 @@
+require "./lib/node"
 class LinkedList
 
   def initialize
+    @head = nil
   end
 
+  def head
+    @head
+  end
+
+  def append(surname)
+    new_node = Node.new(surname)
+    @head = new_node
+  end
 
 end
